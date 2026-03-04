a46e45f83e203ca0355caa5ced63d81fdff9400a
nikozdev <n.zimin@vinteo.ru>
2026-03-04 12:01:44 +0300

    fix: using 0.0.0.0 instead of localhost for docker container to work;

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,7 +18,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", getHandler)
 
-	http.ListenAndServe("localhost:8080", mux);
+	http.ListenAndServe("0.0.0.0:8080", mux);
 
 	log.Println("quit")
 }
