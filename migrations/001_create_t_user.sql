4410963ca7d9e3989cfda037935599cbb259704a
nikozdev <n.zimin@vinteo.ru>
2026-03-04 17:36:52 +0300

    database: adding the created timestamp, changing id to bigserial;

--- migrations/001_create_t_user.sql
+++ migrations/001_create_t_user.sql
@@ -1,7 +1,8 @@
 CREATE TABLE t_user (
-    v_id SERIAL PRIMARY KEY,
+    v_id BIGSERIAL PRIMARY KEY,
     v_name TEXT NOT NULL,
-    v_balance NUMERIC(20,8) NOT NULL DEFAULT 0
+    v_balance NUMERIC(20,8) NOT NULL DEFAULT 0,
+    v_created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
 INSERT INTO t_user (v_name, v_balance) VALUES ('default', 1000.0);
